78113392f7a09b0d5815fc3ab5bd42dea8268482
Serhii-Kondriev <jastech2011@gmail.com>
2024-01-17 23:41:50 +0200

    update hook

--- src/utils/useRandomOutcome.tsx
+++ src/utils/useRandomOutcome.tsx
@@ -4,7 +4,7 @@ const useRandomOutcome = (): [string, () => void] => {
   const [outcome, setOutcome] = useState<string>("");
 
   const generateOutcome = () => {
-    setOutcome(Math.random() < 0.5 ? "Win" : "Lose");
+    setOutcome(Math.random() < 0.5 ? "Victory" : "Defeat");
   };
 
   return [outcome, generateOutcome];
